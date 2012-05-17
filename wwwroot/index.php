9cc84cc78c9034326ec398088148f149e23ef266
sequentialmood <encaps@sequentialmood.net>
2012-05-17 16:22:39 +0200

    GOG
    FOFO

--- wwwroot/index.php
+++ wwwroot/index.php
@@ -6,7 +6,7 @@
 <!--
 //v1.7
 //TEST GIT
-// GOGO
+// GOGO 
 // Flash Player Version Detection
 // Detect Client Browser type
 // Copyright 2005-2008 Adobe Systems Incorporated.  All rights reserved.
